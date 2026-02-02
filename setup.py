5d7b00e12ca251c16055fe7bf499a1299e219e68
Dominik Robak <56892445+dominikrobak@users.noreply.github.com>
2026-02-02 00:12:43 -0500

    Add files via upload
    dependency fixes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/ylla-lab/piRAT',
     author='Dominik Robak',
     author_email='dominikrobak03@gmail.com',
-    packages=find_packages(),  # Add this line
+    packages=find_packages(), 
     entry_points={
         'console_scripts': [
             'pirat=pirat.start:everything',
@@ -17,24 +17,20 @@ setup(
     },
     install_requires=[
         'pysam>=0.22.0',
-        'numpy>=1.20.0',  
         'pandas>=2.0.0',
         'kneed>=0.8.0',
-        'biopython>=1.80', 
         'tqdm>=4.60.0',
         'PyPDF2>=3.0.0',
         'tabulate>=0.9.0',
-        'seqlogo>=5.29.9',
         'logomaker>=0.8',
         'cairosvg>=2.7.0',
         'matplotlib>=3.5.0',
         'imgkit>=1.2.0',
         'scipy>=1.10.0',
-        'matplotlib-venn>=1.1.0', 
         'seaborn>=0.13.0',
         'intervaltree>=3.1.0',
         'pyranges>=0.1.0',
-        'cycler>=0.12.0'
+        'cycler>=0.12.0',
     ],
     python_requires='>=3.8', 
     test_suite='nose.collector',
